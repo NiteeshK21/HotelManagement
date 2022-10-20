990359974f424af1bf12321f40ab89d0a52edb8b
Kshitij Singh <91209593+mekshitijsingh@users.noreply.github.com>
2022-10-21 00:19:24 +0530

    Update HMcode.c

--- HMcode.c
+++ HMcode.c
@@ -257,6 +257,24 @@ void Onlinepayment()
         default: printf("\nInvalid Option!");
     }
 }
+void palin(num){
+	int num, rev=0, rem, temp;
+    cout<<"Enter the Number: ";
+    cin>>num;
+    temp = num;
+    while(temp>0)
+    {
+        rem = temp%10;
+        rev = (rev*10)+rem;
+        temp = temp/10;
+    }
+    if(rev==num)
+        cout<<"\nIt is a Palindrome Number";
+    else
+        cout<<"\nIt is not a Palindrome Number";
+    cout<<endl;
+    return 0;
+}
  void checkout()
  {
      printf("\n\t CUSTOMER CHECKOUT WINDOW");
